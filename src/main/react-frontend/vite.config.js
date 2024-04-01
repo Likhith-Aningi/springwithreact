ed2dd8603a141dd7476a8fe3f37b0f55fbdaf537
Likhith <90026403+Likhith-Aningi@users.noreply.github.com>
2024-04-01 14:48:57 +0530

    Update vite.config.js to generate assets without hash

--- src/main/react-frontend/vite.config.js
+++ src/main/react-frontend/vite.config.js
@@ -5,7 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../resources/static",
+    outDir: "../resources/static",, 
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },  
   },
   
 });
